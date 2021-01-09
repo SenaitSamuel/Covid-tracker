2ba1d7106f895828dc272f4d7d4baa359a4201a2
senaitsamuel <senaitsam@yahoo.com>
2021-01-09 20:27:46 +0100

    init

---
+++ covid19-tracker/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      this is app
+      {/* Header */}
+      {/* Title + select input dropdown field */}
+
+      {/* InforBoxs */}
+      {/* InforBoxs */}
+      {/* InforBoxs */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
+    </div>
+  );
+}
+
+export default App;
